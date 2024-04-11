ffca99b87d59289b61b5c6b012e1f246cd216462
JLdosReis <jldosreisneto@gmail.com>
2024-04-11 14:34:42 -0300

    aula 2 da playlist de fundamentos do professor Pagliares

--- aula4/src/episodio1/TesteHospede.java
+++ aula4/src/episodio1/TesteHospede.java
@@ -1,8 +1,25 @@
 package episodio1;
-
+//é possivel criar estes metodos dentro da classe Hospede
 public class TesteHospede {
 
     public static void main(String[] args) { //pvsm escreve essa linha automaticamente
+        Hospede jose = new Hospede();
+        jose.setNome("José");
+        jose.setSobrenome("Luiz dos Reis Neto");
+
+        //exibindo o estado (refere-se ao conteudo das variaveis que o objeto tera) do objeto
+
+        System.out.println("Nome: " + jose.getNome()); //sout escreve essa linha automaticamente
+        System.out.println("Sobrenome: " + jose.getSobrenome());
+
+        //criando segundo hospede
+
+        Hospede maria = new Hospede();
+        maria.setNome("Maria");
+        maria.setSobrenome("do Carmo Bon Alves");
+
+        System.out.println("Nome: " + maria.getNome());
+        System.out.println("Sobrenome: " + maria.getSobrenome());
 
     }
 
